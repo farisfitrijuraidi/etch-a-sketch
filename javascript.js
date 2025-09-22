3eaadf63284d9c6971ecf3c95e4b27b8a3e3596e
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2025-09-22 09:52:55 +0800

    Add hover effect on each grid

--- javascript.js
+++ javascript.js
@@ -4,3 +4,10 @@ for (let i=0; i<256; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
 }
+
+const grid = document.querySelectorAll('.container div');
+grid.forEach(hover => {
+    hover.addEventListener('mouseenter', (e) => {
+        e.target.style.cssText = 'background: blue;';
+    })
+});
